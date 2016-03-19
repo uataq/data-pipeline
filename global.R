493914c890451e1cf79a1a448c819ca5fbd57573
benfasoli <benfasoli@gmail.com>
2016-03-19 13:19:18 -0600

    Initial commit.

---
+++ global.R
@@ -0,0 +1,17 @@
+reset <- list(
+  csp = F,
+  dbk = F,
+  fru = F, 
+  heb = F,
+  hpl = F,
+  lgn = T,
+  roo = F,
+  rpk = F,
+  sug = F,
+  sun = F,
+  trx01 = F,
+  trx02 = F,
+  wbb = F
+)
+
+globalReset <- F
---
+++ run/sug.R
@@ -0,0 +1,12 @@
+# Ben Fasoli
+
+# Parameters ------------------------------------------------------------------
+site   <- 'lgn'
+ip     <- '166.130.89.167'
+port   <- 3001
+table  <- 'Dat'
+
+# Processing ------------------------------------------------------------------
+source('/uufs/chpc.utah.edu/common/home/lin-group2/measurements/proc/fun/licor.R')
+
+q('no')
