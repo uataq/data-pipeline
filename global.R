8f340f8d4a733b8df984fd8eaf2a674d53651376
benfasoli <benfasoli@gmail.com>
2016-03-19 17:06:28 -0600

    Global function implementation

--- global.R
+++ global.R
@@ -1,3 +1,6 @@
+# Ben Fasoli
+
+# Reset flags -----------------------------------------------------------------
 reset <- list(
   csp = F,
   dbk = F,
@@ -15,3 +18,24 @@ reset <- list(
 )
 
 global_reset <- T
+
+# Functions -------------------------------------------------------------------
+remove_bad <- function(df, site) {
+  bad <- readr::read_csv(paste0('proc/bad/', site, '.txt'), 
+                         locale=locale(tz='UTC'))
+  with(bad, {
+    for (i in 1:length(miu_old)) {
+      if (grepl('all', miu_old[i], ignore.case=T)) {
+        mask <- df$Time_UTC >= t_start[i] & 
+          df$Time_UTC <= t_end[i]
+        df   <- subset(df, !mask)
+      } else {
+        mask <- df$Time_UTC >= t_start[i] & 
+          df$Time_UTC <= t_end[i] &
+          grepl(miu_old[i], df$ID)
+        df$ID[mask] <- miu_new[i]
+      }
+    }
+  })
+  return(df)
+}
