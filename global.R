a6405d2742fe67c34812e12acab16fa9b4aac4c1
benfasoli <benfasoli@gmail.com>
2016-03-21 12:51:04 -0600

    Spacing for readability

--- global.R
+++ global.R
@@ -2,19 +2,19 @@
 
 # Reset flags -----------------------------------------------------------------
 reset <- list(
-  csp = F,
-  dbk = F,
-  fru = F, 
-  heb = F,
-  hpl = F,
-  lgn = F,
-  roo = F,
-  rpk = F,
-  sug = F,
-  sun = F,
+  csp   = F,
+  dbk   = F,
+  fru   = F, 
+  heb   = F,
+  hpl   = F,
+  lgn   = F,
+  roo   = F,
+  rpk   = F,
+  sug   = F,
+  sun   = F,
   trx01 = F,
   trx02 = F,
-  wbb = F
+  wbb   = F
 )
 
 global_reset <- T
