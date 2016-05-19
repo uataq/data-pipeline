58c5a860462e7dbf29b9d191f3d8b3b567b7935d
dougcatharine <doug.catharine@utah.edu>
2016-05-19 13:02:41 -0600

    shutting down IP at castle peak and Roosevelt

--- run/csp.R
+++ run/csp.R
@@ -2,7 +2,7 @@
 
 # Parameters ------------------------------------------------------------------
 site   <- 'csp'
-ip     <- '69.55.97.78'
+#ip     <- '69.55.97.78'
 
 # Processing ------------------------------------------------------------------
 source('/uufs/chpc.utah.edu/common/home/lin-group2/measurements/lair-proc/fun/lgr.R')
--- run/roo.R
+++ run/roo.R
@@ -2,7 +2,7 @@
 
 # Parameters ------------------------------------------------------------------
 site   <- 'roo'
-ip     <- '166.130.125.77'
+#ip     <- '166.130.125.77'
 
 # Processing ------------------------------------------------------------------
 source('/uufs/chpc.utah.edu/common/home/lin-group2/measurements/lair-proc/fun/lgr.R')
