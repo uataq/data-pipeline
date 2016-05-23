3da9832f2a96f31950d977a86776643ae0595076
benfasoli <benfasoli@gmail.com>
2016-05-22 20:38:05 -0600

    Manual site disable no longer required with new run flag.

--- run/csp.R
+++ run/csp.R
@@ -5,6 +5,6 @@ site   <- 'csp'
 ip     <- '69.55.97.78'
 
 # Processing ------------------------------------------------------------------
-# source('/uufs/chpc.utah.edu/common/home/lin-group2/measurements/lair-proc/fun/lgr.R')
+source('/uufs/chpc.utah.edu/common/home/lin-group2/measurements/lair-proc/fun/lgr.R')
 
 q('no')
