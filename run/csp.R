4d9ed9015038d910286ac4c9870258286860edaa
benfasoli <benfasoli@gmail.com>
2016-05-22 20:32:38 -0600

    Disabled csp processing.

--- run/csp.R
+++ run/csp.R
@@ -2,9 +2,9 @@
 
 # Parameters ------------------------------------------------------------------
 site   <- 'csp'
-#ip     <- '69.55.97.78'
+ip     <- '69.55.97.78'
 
 # Processing ------------------------------------------------------------------
-source('/uufs/chpc.utah.edu/common/home/lin-group2/measurements/lair-proc/fun/lgr.R')
+# source('/uufs/chpc.utah.edu/common/home/lin-group2/measurements/lair-proc/fun/lgr.R')
 
 q('no')
