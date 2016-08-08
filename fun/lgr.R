6821b889deb1f90dc3b99e60141abdd972261103
benfasoli <benfasoli@gmail.com>
2016-08-08 13:07:21 -0600

    Added rmse of cal fit

--- fun/lgr.R
+++ fun/lgr.R
@@ -12,7 +12,7 @@ try({
   library(dplyr,   lib.loc=lib)
   library(readr,   lib.loc=lib)
   library(uataq,   lib.loc=lib)
-  
+
   # Functions -------------------------------------------------------------------
   pull_lgr <- function(ip, site) {
     cmd <- paste0('/usr/bin/rsync -vrutzhO --stats --exclude="archive/" -e ',
@@ -21,26 +21,26 @@ try({
                   '/uufs/chpc.utah.edu/common/home/lin-group2/measurements/data/', site, '/raw/')
     system(print(cmd, quote=F))
   }
-  
-  
+
+
   # Directory structure and data ------------------------------------------------
   check_bad()
-  
+
   if (reset[[site]] | global_reset) {
-    system(paste0('rm ', 'data/', site, '/parsed/*')) 
+    system(paste0('rm ', 'data/', site, '/parsed/*'))
     system(paste0('rm ', 'data/', site, '/calibrated/*'))
-    dir.create(file.path('data', site, 'parsed'), 
+    dir.create(file.path('data', site, 'parsed'),
                showWarnings=FALSE, recursive=TRUE, mode='0755')
-    dir.create(file.path('data', site, 'calibrated'), 
+    dir.create(file.path('data', site, 'calibrated'),
                showWarnings=FALSE, recursive=TRUE, mode='0755')
-    
+
     cal_all <- T
   } else {
     if (!run[[site]]) stop('Site processing disabled in global.R')
     cal_all <- F
     pull_lgr(ip, site)
   }
-  
+
   # Determine files to be read --------------------------------------------------
   # Unzip necessary compressed data packets
   dir(file.path('data', site, 'raw'), '\\.{1}txt\\.{1}zip', full.names=T, recursive=T) %>%
@@ -55,9 +55,9 @@ try({
         system(paste('unzip -o', zf, '-d', dirname(zf)))
         system(paste('touch', zf, tf))
       }
-    }) %>% 
+    }) %>%
     invisible()
-  
+
   tfs <- dir(file.path('data', site, 'raw'), 'f....\\.{1}txt$', full.names=T, recursive=T)
   if (!cal_all) {
     # Sort by file modification time since older versions of LGR's software
@@ -65,16 +65,16 @@ try({
     tfs <- tfs[order(file.mtime(tfs))]
     tfs <- tail(tfs, 2)
   }
-  
+
   # Read files ------------------------------------------------------------------
   raw <- lapply(tfs, function(tf) {
     df <- tryCatch(suppressWarnings(
-      read_csv(tf, col_names=F, skip=2, na=c('TO', '', 'NA'), 
+      read_csv(tf, col_names=F, skip=2, na=c('TO', '', 'NA'),
                locale=locale(tz='UTC'))),
       error=function(e){NULL})
-    
+
     if(is.null(df) || ncol(df) < 23 || ncol(df) > 24) return(NULL)
-    
+
     # Adapt column names depending on LGR software version.
     #  2013-2014 version has 23 columns.
     #  2014+ version has 24 columns (MIU split into valve and description).
@@ -92,14 +92,14 @@ try({
                                     'AmbT_C_sd', 'RD0_us', 'RD0_us_sd', 'RD1_us',
                                     'RD1_us_sd', 'Fit_Flag', 'MIU_v', 'ID'))
     dplyr::filter(df, !is.na(ID))
-  }) %>% 
+  }) %>%
     bind_rows() %>%
     mutate(Time_UTC = as.POSIXct(Time_UTC, tz='UTC', format='%m/%d/%Y %H:%M:%S')) %>%
     filter(!is.na(Time_UTC)) %>%
     arrange(Time_UTC)
-  
+
   if ('MIU_v' %in% names(raw)) raw <- select(raw, -MIU_v)
-  
+
   # UTC Changeover --------------------------------------------------------------
   # Remove data during 12-29-2015 to 12-30-2015 during which the sites were
   # being changed from local time to UTC.
@@ -107,13 +107,13 @@ try({
     filter(Time_UTC < as.POSIXct('2015-12-29', tz='UTC') |
              Time_UTC > as.POSIXct('2015-12-30', tz='UTC')) %>%
     mutate(pre_utc = Time_UTC < as.POSIXct('2015-12-30', tz='UTC'))
-  
-  raw$Time_UTC[raw$pre_utc] <- as.POSIXct(format(raw$Time_UTC[raw$pre_utc], 
+
+  raw$Time_UTC[raw$pre_utc] <- as.POSIXct(format(raw$Time_UTC[raw$pre_utc],
                                                  tz='UTC'), tz='America/Denver')
-  
+
   # Remove bad data -------------------------------------------------------------
   # remove_bad() found in global.R
-  parsed <- raw %>% 
+  parsed <- raw %>%
     select(-pre_utc) %>%
     filter(!duplicated(Time_UTC)) %>%
     remove_bad(site) %>%
@@ -125,47 +125,49 @@ try({
            ID = gsub('unknown', NA, ID, ignore.case=T),
            site_id = site) %>%
     filter(!is.na(ID))
-  
+
   ID_split <- stringr::str_split_fixed(parsed$ID, '~', 2)
   ID_split[grepl('atmosphere', ID_split[ ,1], ignore.case=T)] <- '-10'
   ID_split[grepl('flush', ID_split[ ,1], ignore.case=T)]      <- '-99'
   ID_split <- matrix(as.numeric(ID_split), ncol=2)
-  
+
   parsed$ID_co2 <- ID_split[ ,1]
   parsed$ID_ch4 <- ID_split[ ,2]
-  
+
   uataq::archive(parsed, path=file.path('data', site, 'parsed/%Y_%m_parsed.dat'))
-  
+
   # Calibrations ----------------------------------------------------------------
   if (!cal_all) {
     files <- tail(dir(file.path('data', site, 'parsed'), full.names=T), 2)
     parsed <- lapply(files, read_csv, locale=locale(tz='UTC'),
                      col_types='T------d-d--------------dd') %>% bind_rows()
   }
-  
-  cal_co2 <- with(parsed, 
+
+  cal_co2 <- with(parsed,
                   uataq::calibrate(Time_UTC, CO2d_ppm, ID_co2,
                                    auto=T, er_tol=0.15, dt_tol=18000))
-  
-  cal_ch4 <- with(parsed, 
+
+  cal_ch4 <- with(parsed,
                   uataq::calibrate(Time_UTC, CH4d_ppm, ID_ch4,
                                    auto=T, er_tol=0.15, dt_tol=18000))
-  
+
   cal <- data_frame(Time_UTC     = cal_co2$time,
                     CO2d_ppm_cal = cal_co2$cal,
                     CO2d_ppm_raw = cal_co2$raw,
                     m_co2        = cal_co2$m,
                     b_co2        = cal_co2$b,
                     n_co2        = cal_co2$n,
+                    rmse_co2     = cal_co2$rmse,
                     CH4d_ppm_cal = cal_ch4$cal,
                     CH4d_ppm_raw = cal_ch4$raw,
                     m_ch4        = cal_ch4$m,
                     b_ch4        = cal_ch4$b,
-                    n_ch4        = cal_ch4$n) %>%
+                    n_ch4        = cal_ch4$n,
+                    rmse_ch4     = cal_ch4$rmse) %>%
     filter(n_co2 > 0 | n_ch4 > 0) %>%
     mutate(site_id = site)
-  
-  uataq::archive(cal, path=file.path('data', site, 
+
+  uataq::archive(cal, path=file.path('data', site,
                                      'calibrated/%Y_%m_calibrated.dat'))
 })
 lock_remove()
--- fun/licor.R
+++ fun/licor.R
@@ -13,7 +13,7 @@ try({
   library(readr,   lib.loc=lib)
   library(rPython, lib.loc=lib)
   library(uataq,   lib.loc=lib)
-  
+
   # Functions -------------------------------------------------------------------
   get_cr1000 <- function(ip, port, table, site){
     # Collects data from a remote CR1000, using python.    Ben Fasoli
@@ -21,15 +21,15 @@ try({
     #     port         port number
     #     table        CR1000 table to download
     require(rPython)
-    
-    lastf <- tail(dir(file.path('data', site, 'raw'), 
+
+    lastf <- tail(dir(file.path('data', site, 'raw'),
                       pattern='.*\\.{1}dat', full.names=T), 1)
-    
+
     if(length(lastf)>0) {
-      t_start <- system(paste('tail -n 1', lastf), intern = T) %>% 
-        uataq::breakstr() %>% 
-        select(1) %>% 
-        as.character() %>% 
+      t_start <- system(paste('tail -n 1', lastf), intern = T) %>%
+        uataq::breakstr() %>%
+        select(1) %>%
+        as.character() %>%
         as.POSIXct(tz = 'UTC', format = '%Y-%m-%d %H:%M:%S')
       t_start <- as.character(t_start + 1)
       t_end   <- format((Sys.time() + 86400), tz='UTC')
@@ -37,20 +37,20 @@ try({
       t_start <- ''
       t_end   <- ''
     }
-    
+
     python.load('lair-proc/fun/licor_init.py')
     raw <- python.call('crpull', ip, port, table, t_start, t_end)
-    
+
     if(all(is.null(raw)) | length(raw) < 2) stop('No new data found on logger.')
-    
+
     data <- uataq::breakstr(raw, ncol=21)
     colnames(data) <- c('Time_UTC', 'n', 'Year', 'jDay', 'HH', 'MM', 'SS',
                         'batt_volt', 'PTemp', 'Room_T', 'IRGA_T', 'IRGA_P',
                         'MF_Controller_mLmin', 'PressureVolt', 'rhVolt', 'gas_T',
                         'rawCO2_Voltage', 'rawCO2', 'rawH2O', 'ID', 'Program')
     types <- c('POSIXct', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric',
-               'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 
-               'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 
+               'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric',
+               'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric',
                'numeric', 'numeric', 'character')
     for (i in 1:ncol(data)) {
       fun <- switch(types[i],
@@ -60,29 +60,29 @@ try({
                       as.POSIXct(x, tz='UTC', format='%Y-%m-%d %H:%M:%S')})
       data[[i]] <- fun(data[[i]])
     }
-    
-    uataq::archive(data, path=file.path('data', site, 'raw/%Y_%m_raw.dat'), 
+
+    uataq::archive(data, path=file.path('data', site, 'raw/%Y_%m_raw.dat'),
                    col_names=F)
     return(data)
   }
-  
+
   # Directory structure and data ------------------------------------------------
   check_bad()
   if (reset[[site]] | global_reset) {
-    system(paste0('rm ', 'data/', site, '/parsed/*')) 
+    system(paste0('rm ', 'data/', site, '/parsed/*'))
     system(paste0('rm ', 'data/', site, '/calibrated/*'))
-    dir.create(file.path('data', site, 'parsed'), 
+    dir.create(file.path('data', site, 'parsed'),
                showWarnings=FALSE, recursive=TRUE, mode='0755')
-    dir.create(file.path('data', site, 'calibrated'), 
+    dir.create(file.path('data', site, 'calibrated'),
                showWarnings=FALSE, recursive=TRUE, mode='0755')
-    
+
     cal_all <- T
-    
+
     raw <- dir(paste0('data/', site, '/raw'), full.names=T) %>%
       lapply(read_csv, locale=locale(tz='UTC'),
              col_names=c('Time_UTC', 'n', 'Year', 'jDay', 'HH', 'MM', 'SS',
                          'batt_volt', 'PTemp', 'Room_T', 'IRGA_T', 'IRGA_P',
-                         'MF_Controller_mLmin', 'PressureVolt', 'rhVolt', 
+                         'MF_Controller_mLmin', 'PressureVolt', 'rhVolt',
                          'gas_T', 'rawCO2_Voltage', 'rawCO2', 'rawH2O',
                          'ID', 'Program'),
              col_types='Tdddddddddddddddddddc') %>%
@@ -94,22 +94,22 @@ try({
     raw <- get_cr1000(ip, port, table, site)
   }
   raw$ID <- round(raw$ID, 2)
-  
+
   # User defined bad data -------------------------------------------------------
   parsed <- remove_bad(raw, site) %>%
     mutate(ID_co2 = ID,
            CO2d_ppm = rawCO2,
            site_id = site)
   uataq::archive(parsed, path=file.path('data', site, 'parsed/%Y_%m_parsed.dat'))
-  
+
   # Calibrations ----------------------------------------------------------------
   if (!cal_all) {
     files <- tail(dir(file.path('data', site, 'parsed'), full.names=T), 2)
     parsed <- lapply(files, read_csv, locale=locale(tz='UTC'),
                      col_types='T--------------------dd-') %>% bind_rows()
   }
-  
-  cal <- with(parsed, 
+
+  cal <- with(parsed,
               uataq::calibrate(Time_UTC, CO2d_ppm, ID_co2,
                                auto=T, er_tol=0.15, dt_tol=18000)) %>%
     rename(Time_UTC     = time,
@@ -117,11 +117,12 @@ try({
            CO2d_ppm_raw = raw,
            m_co2        = m,
            b_co2        = b,
-           n_co2        = n) %>%
+           n_co2        = n,
+           rmse_co2     = rmse) %>%
     filter(n_co2 > 0) %>%
     mutate(site_id = site)
-  
-  uataq::archive(cal, path=file.path('data', site, 
+
+  uataq::archive(cal, path=file.path('data', site,
                                      'calibrated/%Y_%m_calibrated.dat'))
 })
 lock_remove()
