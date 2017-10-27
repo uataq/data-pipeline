a9ab224937a43febce6778a3bcccb693c2849e4c
Ben Fasoli <benfasoli@gmail.com>
2017-10-26 18:45:54 -0600

    Begin migration

--- global.r
+++ global.r
@@ -43,47 +43,6 @@ global_reset <- F
 
 
 # Functions -------------------------------------------------------------------
-remove_bad <- function(df, site) {
-  bad <- readr::read_csv(paste0('lair-proc/bad/', site, '.txt'),
-                         locale=locale(tz='UTC'), col_types = 'TTcc_')
-  for (i in 1:nrow(bad)) {
-    if (grepl('all', bad$miu_old[i], ignore.case=T)) {
-      mask <- df$Time_UTC >= bad$t_start[i] &
-        df$Time_UTC <= bad$t_end[i]
-    } else {
-      mask <- df$Time_UTC >= bad$t_start[i] &
-        df$Time_UTC <= bad$t_end[i] &
-        grepl(bad$miu_old[i], df$ID)
-    }
-    df$ID[mask] <- bad$miu_new[i]
-  }
-  return(df)
-}
-
-# Bad data reset
-check_bad <- function() {
-  badf <- dir('lair-proc/bad', pattern=site, full.names=T)
-  mt <- badf %>%
-    file.info %>%
-    .$mtime
-
-  mt_df <- readRDS('lair-proc/bad/_log.rds')
-  if (trunc(mt) != trunc(mt_df[site, 'mtime'])) {
-    mt_df[site, 'mtime'] <- mt
-    saveRDS(mt_df, 'lair-proc/bad/_log.rds')
-    reset[[site]] <<- T
-
-    # Generate initial:
-    #     badf <- dir('bad', pattern='txt', full.names=T)
-    #     mt <- badf %>%
-    #       file.info %>%
-    #       .$mtime
-    #     df <- data.frame(stringsAsFactors=F, mtime = mt)
-    #     rownames(df) <- basename(tools::file_path_sans_ext(badf))
-    #     df$site <- NULL
-    #     saveRDS(df, 'lair-proc/bad/_log.rds')
-  }
-}
 
 # Lock file generation and management
 lock_create <- function() {
@@ -95,4 +54,17 @@ lock_create <- function() {
 lock_remove <- function() {
   lockfile <- paste0('lair-proc/lock/', site, '.running')
   system(paste('rm', lockfile))
+}
+
+
+
+# Remote connection and data querying
+query_cr1000 <- function(ip, table, t_start) {
+  t_start <- format(t_start, tz = 'UTC', '%Y-%m-%dT%H:%M:%S')
+  uri <- paste0('https://air.utah.edu/api/fetch_cr1000/?ip=', ip, '&table=', table,
+                '&t_start=', t_start)
+  message('Querying: ', uri)
+  response <- scan(uri, character(), sep = '\n', quiet = T)
+  data <- read.table(text = response, sep = ',', skip = 4, stringsAsFactors = F)
+  colnames(data) <- scan(text = response[2], what = character(), sep = ',', quiet = T)
 }
