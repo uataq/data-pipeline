57965ab0b9fb9132c197a187a57f1ae5788b6609
Ben Fasoli <benfasoli@gmail.com>
2018-03-21 22:30:04 -0600

    Add fst as secondary archive method

--- src/update_archive.r
+++ src/update_archive.r
@@ -1,18 +1,19 @@
-update_archive <- function(nd, path = '%Y_%m.dat', tz = 'UTC') {
-  
+update_archive <- function(nd, path = '%Y_%m.dat', tz = 'UTC', as_fst = F) {
+
   if (nrow(nd) < 1)
     stop('No new data to append.')
-  
+
   if (!dir.exists(dirname(path)))
     dir.create(dirname(path), recursive = T, mode = '0755')
-  
+
   time_col <- grep('time', names(nd), ignore.case = T, value = T)[1]
-  
+
   nd <- nd[!is.na(nd[[time_col]]), ]
   nd <- nd[order(nd[[time_col]]), ]
   files <- format(nd[[time_col]], tz = tz, format = path)
+  ufiles <- unique(files)
   
-  for (file in (unique(files))) {
+  for (file in (ufiles)) {
     mask <- file == files
     out <- nd[mask, ]
     append <- file.exists(file)
@@ -24,7 +25,23 @@ update_archive <- function(nd, path = '%Y_%m.dat', tz = 'UTC') {
     }
     if (nrow(out) < 1)
       next
-    out[[time_col]] <- format(out[[time_col]], tz = tz, '%Y-%m-%dT%H:%M:%OS2')
+    out[[time_col]] <- format(out[[time_col]], tz = tz, '%Y-%m-%d %H:%M:%OS2')
     write_csv(out, file, append = append)
   }
+
+  if (!as_fst) return()
+  invisible(threads_fst(4))
+  
+  fst_base <- dirname(path)
+  fst_dir <- dirname(fst_base)
+  fst_file <- file.path(fst_dir, paste0(basename(fst_base), '.fst'))
+
+  if (!file.exists(fst_file)) {
+    fst_data <- NULL
+  } else {
+    fst_data <- read_fst(fst_file)
+    nd <- nd[nd$Time_UTC > tail(fst_data$Time_UTC, 1), ]
+  }
+  fst_data <- bind_rows(fst_data, nd)
+  write_fst(fst_data, fst_file, compress = 50)
 }
