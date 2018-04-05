b55d1aa6b1af531c14d6ace02e742de8766e03e4
Ben Fasoli <benfasoli@gmail.com>
2018-04-05 14:40:15 -0600

    Dynamically identify time column

--- src/read_files.r
+++ src/read_files.r
@@ -1,5 +1,6 @@
 read_files <- function(files, ...) {
   df <- rbindlist(lapply(files, fread, data.table = F, showProgress = F, ...))
-  df$Time_UTC <- fastPOSIXct(df$Time_UTC, tz = 'UTC')
+  time_col <- grep('time', colnames(df), ignore.case = T, value = T)[1]
+  df[[time_col]] <- fastPOSIXct(df[[time_col]], tz = 'UTC')
   return(df)
 }
