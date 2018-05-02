43b6632d9abca3e3e6bc7aa72a3755caad724909
Ben Fasoli <benfasoli@gmail.com>
2018-05-02 15:50:58 -0600

    IP address migrated to USU modem

--- run/roo.r
+++ run/roo.r
@@ -2,7 +2,7 @@
 
 # Parameters ------------------------------------------------------------------
 site   <- 'roo'
-ip     <- '166.130.125.77'
+ip     <- '67.213.235.123'
 
 # Processing ------------------------------------------------------------------
 source('/uufs/chpc.utah.edu/common/home/lin-group2/measurements/lair-proc/fun/lgr-ugga.r')
