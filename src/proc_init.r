6eb259aa13c06480d928738631e85cf5a2a48f93
Ben Fasoli <benfasoli@gmail.com>
2018-07-26 10:32:49 -0600

    Set reprocess flag earlier if necessary

--- src/proc_init.r
+++ src/proc_init.r
@@ -7,6 +7,12 @@ proc_init <- function() {
 
   # Check if bad data file has been modified since last run
   bad_log_check()
+  
+  # Ensure that past processed data exists and set reprocess flag as needed
+  for (path in file.path(wd, c('qaqc', 'calibrated'))) {
+    if (!file.exists(path) || length(dir(path)) == 0)
+      site_info[[site]]$reprocess <<- T
+  }
 
   # Check if reprocess flag is TRUE. If site/instrument archive needs to be
   # reprocessed, remove parsed and calibrated data levels
@@ -15,12 +21,6 @@ proc_init <- function() {
     for (path in file.path(wd, c('qaqc', 'calibrated'))) {
       system(paste('rm -r', path))
     }
-  }
-
-  # Ensure that past processed data exists and set reprocess flag as needed
-  for (path in file.path(wd, c('qaqc', 'calibrated'))) {
-    if (!file.exists(path) || length(dir(path)) == 0)
-      site_info[[site]]$reprocess <<- T
   }
 
   # Check if site is currently active. Exit processing if data is up to date
