bc7e6b0ed1b4c555516fbf597aca14c7e9a7d4f7
Ben Fasoli <benfasoli@gmail.com>
2018-07-26 10:32:26 -0600

    Allow disabling of file header check

--- src/update_archive.r
+++ src/update_archive.r
@@ -1,4 +1,4 @@
-update_archive <- function(nd, path = '%Y_%m.dat', tz = 'UTC') {
+update_archive <- function(nd, path = '%Y_%m.dat', tz = 'UTC', check_header = T) {
 
   if (nrow(nd) < 1)
     stop('No new data to append.')
@@ -7,10 +7,11 @@ update_archive <- function(nd, path = '%Y_%m.dat', tz = 'UTC') {
     dir.create(dirname(path), recursive = T, mode = '0755')
 
   time_col <- grep('time', names(nd), ignore.case = T, value = T)[1]
-
+  if (is.na(time_col)) time_col <- 1
+  
   nd <- nd[!is.na(nd[[time_col]]), ]
   nd <- nd[order(nd[[time_col]]), ]
-  nd <- nd[!duplicated(nd$Time_UTC), ]
+  nd <- nd[!duplicated(nd[[time_col]]), ]
   files <- format(nd[[time_col]], tz = tz, format = path)
   ufiles <- unique(files)
   
@@ -20,7 +21,7 @@ update_archive <- function(nd, path = '%Y_%m.dat', tz = 'UTC') {
     append <- file.exists(file)
     if (append) {
       hdr <- get_file_header(file)
-      if (!all(hdr == colnames(out)))
+      if (check_header && !all(hdr == colnames(out)))
         stop('Data structure has changed and headers now conflict.')
       out <- out[out[[time_col]] > get_last_time(file), ]
     }
