00cd9b4e9ba049d90607e855c081936ebd6619fa
Ben Fasoli <benfasoli@gmail.com>
2018-08-29 14:00:00 -0600

    Only report lockfile status for interactive sessions
    Avoids refreshing log file when another script is writing to it

--- src/lock_create.r
+++ src/lock_create.r
@@ -2,8 +2,12 @@ lock_create <- function(site = get('site', envir = globalenv())) {
   
   lockfile <- file.path('pipeline', '.lock', paste0(site, '.lock'))
   
-  if (file.exists(lockfile))
-    stop(paste(site, 'processing running and locked. Exiting...'))
+  if (file.exists(lockfile)) {
+    if (interactive()) {
+      stop(paste(site, 'processing running and locked. Exiting...'))
+    }
+    q('no')
+  }
   
   system(paste('touch', lockfile))
 }
