25804855655581d94020ac612b7588edf2d1625d
Ben Fasoli <benfasoli@gmail.com>
2020-12-22 14:17:29 -0700

    Configure sync from TRX02

---
+++ run/trx02.r
@@ -0,0 +1,27 @@
+# Ben Fasoli
+
+site   <- 'trx02'
+
+# Load settings and initialize lock file
+source('/uufs/chpc.utah.edu/common/home/lin-group9/measurements/pipeline/_global.r')
+site_config <- site_config[site_config$stid == site, ]
+
+lock_create()
+
+if (!site_config$reprocess && 
+    !cr1000_is_online(paste(sep=':', site_config$ip, 9191))) {
+  lock_remove()
+  stop('Unable to connect to ', site_config$ip)
+}
+
+try({
+  instrument <- 'teledyne_t500u'
+  proc_init()
+  
+  path <- file.path('data', site, instrument, 'raw')
+  remote <- paste0('pi@', site_config$ip, ':/home/pi/data/teledyne_t500u/')
+  local <- file.path('data', site, 'raw', instrument)
+  rsync(from = remote, to = local, port = site_config$port)
+})
+
+lock_remove()
