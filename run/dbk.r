0c5f041e7cfab8e8d164d34c5633b95aa5cc8ef5
John-C-Lin <John.Lin@utah.edu>
2022-05-20 09:19:15 -0600

    Use instrument.ghg specified in site_config.csv to determine which instrument measuring GHG at DBK, and call relevant scripts

--- run/dbk.r
+++ run/dbk.r
@@ -10,12 +10,14 @@ lock_create()
 try({
   # Licor 6262 -----------------------------------------------------------------
   # instrument <- 'licor_6262'
-  instrument <- 'licor_7000'   # switch to licor_7000 in May 2022
+  #instrument <- 'licor_7000'   # switch to licor_7000 in May 2022
+  instrument <- site_config$instrument.ghg   # 'licor_6262', 'licor_7000', or 'lgr_ugga'
   proc_init()
   nd <- cr1000_init()
  if (!site_config$reprocess)
     update_archive(nd, data_path(site, instrument, 'raw'), check_header = F)
-  nd <- licor_7000_qaqc()
+  if(instrument=='licor_6262') nd <- licor_6262_qaqc()
+  if(instrument=='licor_7000') nd <- licor_7000_qaqc()
   update_archive(nd, data_path(site, instrument, 'qaqc'))
   # calibration code for licor_7000 is the same as for licor_6262, so just call licor_6262_calibrate()
   nd <- licor_6262_calibrate()
