132dc97d1a75c449b5a0c2f43a274b643aeac88c
James Mineau <60365712+jmineau@users.noreply.github.com>
2023-06-27 18:41:47 -0600

    bug in checking if site uses pi

--- src/lgr_ugga_qaqc.r
+++ src/lgr_ugga_qaqc.r
@@ -1,6 +1,7 @@
 lgr_ugga_qaqc <- function() {
-  
-  if (!grepl('trx'|'ldf', site)) {  # if not a raspberry pi site
+
+  pi_sites <- c('trx01', 'ldf')
+  if (!(site %in% pi_sites)) {
     # Standardize field names
     colnames(nd) <- data_config[[instrument]]$qaqc$col_names[1:ncol(nd)]
     
