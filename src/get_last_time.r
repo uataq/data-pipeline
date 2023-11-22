e1462c57cc3dd82e35507f9cbd050a3453376e9f
James Mineau <jameskmineau@gmail.com>
2023-11-22 09:36:55 -0700

    check for 0 char

--- src/get_last_time.r
+++ src/get_last_time.r
@@ -1,8 +1,8 @@
 get_last_time <- function(file, timecol = 1, tz = 'UTC', ...) {
   
-  if (is.null(file) || !file.exists(file)) {
+  if (is.null(file) || !file.exists(file) || length(file) == 0) {
     message('No file found for extracting last time at: ', file)
-    return(as.POSIXct(0, tz = 'UTC', origin = '1970-01-01'))
+    return(as.POSIXct(0, tz = 'UTC', origin = '2000-01-01'))
   }
 
   last <- unlist(strsplit(system(paste('tail -n 1', file), intern = T), ','))
